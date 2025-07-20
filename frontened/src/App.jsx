07c44e3961c412070acb39af9bc56813172bc7e2
Aman-patel-18 <amanp7381@gmail.com>
2025-07-20 16:57:35 +0530

    Installing shadcn ui

--- frontened/src/App.jsx
+++ frontened/src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Button } from "./components/ui/button"
+import { Ghost } from "lucide-react";
+
 
 const App = () => {
-  return <h1 class="text-3xl font-bold underline text-red-500">
+  return(
+  <div><h1 class="text-3xl font-bold underline text-red-500">
     Hello world!
   </h1>
+  <Button className="flex flex-col items-center justify-center bg-red-500" variant={Ghost}>
+    Click me
+    </Button>
+  </div>
+  )
 };
 
 export default App;
