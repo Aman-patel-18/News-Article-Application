2d6b56917f3d83635988b46ca722b5d0d3d1e96a
Aman-patel-18 <amanp7381@gmail.com>
2025-07-20 18:34:42 +0530

    add pages and routes

--- frontened/src/App.jsx
+++ frontened/src/App.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Button } from "./components/ui/button"
-import { Ghost } from "lucide-react";
+import { Ghost, Home } from "lucide-react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignInForm from "./auth/forms/SignInForm";
+import SignUpForm from "./auth/forms/SignUpForm";
+import Dashboard from "./pages/Dashboard";
+import NewsArticles from "./pages/NewsArticles";
 
 
 const App = () => {
   return(
-  <div><h1 class="text-3xl font-bold underline text-red-500">
-    Hello world!
-  </h1>
-  <Button className="flex flex-col items-center justify-center bg-red-500" variant={Ghost}>
-    Click me
-    </Button>
-  </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/sign-in" element={<SignInForm/>}></Route>
+      <Route path="/sign-up" element={<SignUpForm/>}></Route>
+
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/dashboard" element={<Dashboard/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/news" element={<NewsArticles/>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 };
 
