085bd508604070ceabdc26b28fd3c9ec590741e2
Aman-patel-18 <amanp7381@gmail.com>
2025-07-21 16:35:37 +0530

    added navbar

--- frontened/src/App.jsx
+++ frontened/src/App.jsx
@@ -1,27 +1,30 @@
 import React from "react";
-import { Button } from "./components/ui/button"
-import { Ghost, Home } from "lucide-react";
+import { Button } from "./components/ui/button";
+// import { Ghost, Home} from "lucide-react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignInForm from "./auth/forms/SignInForm";
 import SignUpForm from "./auth/forms/SignUpForm";
+import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import NewsArticles from "./pages/NewsArticles";
-
+import About from "./pages/About";
+import Header from "./components/shared/Header";
 
 const App = () => {
-  return(
+  return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/sign-in" element={<SignInForm/>}></Route>
-      <Route path="/sign-up" element={<SignUpForm/>}></Route>
+    <Header/>
+      <Routes>
+        <Route path="/sign-in" element={<SignInForm />}></Route>
+        <Route path="/sign-up" element={<SignUpForm />}></Route>
 
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/dashboard" element={<Dashboard/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-      <Route path="/news" element={<NewsArticles/>}></Route>
-    </Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/news" element={<NewsArticles />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 };
 
 export default App;
