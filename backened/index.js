4709836d9f2ec7a619e06ed74df18641c1ab8321
Aman-patel-18 <amanp7381@gmail.com>
2025-07-23 15:19:56 +0530

    Added middleware to handle possible errors

--- backened/index.js
+++ backened/index.js
@@ -18,7 +18,18 @@ mongoose
     console.log(err);
   });
 
-app.use("/api/auth", authRoutes); // ✅ Routes after middleware
+app.use("/api/auth", authRoutes);
+
+app.use((err,req, res, next)=>{
+  const statusCode = err.statusCode || 500
+
+  const message=err.message || "Internal Server Error"
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
 
 app.listen(3000, () => {
   console.log("server is running!");
