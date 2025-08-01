3bdc743b9ccc6c64acd84a1cfadae2af24952efc
Aman-patel-18 <amanp7381@gmail.com>
2025-08-01 16:03:02 +0530

    added update user api route

--- backened/index.js
+++ backened/index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
 const app = express(); // ✅ Declare app first
 
 app.use(express.json()); // ✅ Now this works correctly
+app.use(cookieParser())
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -19,6 +22,7 @@ mongoose
   });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.use((err,req, res, next)=>{
   const statusCode = err.statusCode || 500
