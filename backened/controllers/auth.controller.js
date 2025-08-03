80fd14161bdf008c752df5e3894d8bd41e03f6b0
Aman-patel-18 <amanp7381@gmail.com>
2025-08-03 16:31:11 +0530

    admin field added

--- backened/controllers/auth.controller.js
+++ backened/controllers/auth.controller.js
@@ -48,7 +48,7 @@ export const signin=async(req, res,next)=>{
                 return next(errorHandler(400, "Wrong Credentials"))
             }
 
-            const token=jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+            const token=jwt.sign({id: validUser._id, isAdmin: validUser.isAdmin}, process.env.JWT_SECRET)
             const {password:pass, ...rest}=validUser._doc
             res.status(200).cookie("access_token", token, {httpOnly: true}).json(rest)
         } catch (error) {
@@ -62,7 +62,7 @@ export const google = async(req,res,next)=>{
         const user=await User.findOne({email})
         //signin condition
         if(user){
-            const token=jwt.sign({id: user._id}, process.env.JWT_SECRET)
+            const token=jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET)
             const {password: pass, ...rest}=user._doc
             return res.status(200).cookie("access_token", token, {
                 httpOnly:true,
@@ -88,7 +88,7 @@ export const google = async(req,res,next)=>{
         })
         await newUser.save()
         
-        const token=jwt.sign({id: newUser._id}, process.env.JWT_SECRET)
+        const token=jwt.sign({id: newUser._id, isAdmin:newUser.isAdmin}, process.env.JWT_SECRET)
         const {password: pass, ...rest}=newUser._doc
         
         return res
